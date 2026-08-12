f5e0101cc0f13b9b8067723455c9c61bfd618ef8
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2026-08-13 02:03:34 +0800

    migrate af2026 mob to new API

--- OpenTaiko/System/Open-World Memories/Graphics/5_Game/3_Mob/A2_af2026/Script.lua
+++ OpenTaiko/System/Open-World Memories/Graphics/5_Game/3_Mob/A2_af2026/Script.lua
@@ -1,10 +1,5 @@
---func:DrawText(x, y, text)
---func:DrawNum(x, y, num)
---func:AddGraph("filename")
---func:DrawGraph(x, y, filename)
---func:SetOpacity(opacity, "filename")
---func:SetScale(xscale, yscale, "filename")
---func:SetColor(r, g, b, "filename")
+---@diagnostic disable: undefined-global  -- TEXTURE/fps injected by CLuaScript at runtime
+-- Ported from the old ScriptBG func: API to the ROActivity LuaTexture API.
 
 --local debug_counter = 0
 
@@ -20,26 +15,28 @@ local mob_out_counter = 0
 
 local mob_state = 0
 
-function mobIn()
+local tx = {}
+
+local function mobIn()
     mob_state = 1
     mob_in_counter = 0
 end
 
-function mobOut()
+local function mobOut()
     mob_state = 2
     mob_out_counter = 0
 end
 
-function init()
-    func:AddGraph("Mob_Front.png")
-    func:AddGraph("Mob_Back.png")
-    func:AddGraph("Mob_Back2.png")
-    mob_height = func:GetTextureHeight("Mob_Front.png")
+function onStart()
+    tx["Mob_Front.png"] = TEXTURE:CreateTextureSync("Mob_Front.png")
+    tx["Mob_Back.png"] = TEXTURE:CreateTextureSync("Mob_Back.png")
+    tx["Mob_Back2.png"] = TEXTURE:CreateTextureSync("Mob_Back2.png")
+    mob_height = tx["Mob_Front.png"].Height
 end
 
-function update()
+function update(timestamp, state)
 
-    --debug_counter = debug_counter + (deltaTime)
+    --debug_counter = debug_counter + (fps.deltaTime)
 
     --if debug_counter > 2 then
     --    if mob_state == 0 then
@@ -52,26 +49,29 @@ function update()
 
 
 
-    if mob_state == 3 and gauge[0] < 100 then
+    if mob_state == 3 and state.gauge[0] < 100 then
         mobOut()
     end
 
     if mob_state == 0 then
 
-        if gauge[0] == 100 then
+        if state.gauge[0] == 100 then
             mobIn()
         end
 
     elseif mob_state == 1 then
 
-        mob_in_counter = mob_in_counter + (bpm[0] * deltaTime / 30.0)
-        if mob_in_counter > 1 then
-            mob_state = 3
-            mob_counter = 0.5
-            mob_action_counter = 0
+        local value = math.min(1, mob_in_counter + (2 * math.abs(state.bpm[0]) * fps.deltaTime / 60.0))
+        if 0 * value == 0 * value then  -- finite
+            mob_in_counter = value
+            if value >= 1 then
+                mob_state = 3
+                mob_counter = 0.5
+                mob_action_counter = 0
+            end
         end
 
-        mobinValue = (1.0 - math.sin(mob_in_counter * math.pi / 2))
+        local mobinValue = (1.0 - math.sin(mob_in_counter * math.pi / 2))
         mob_front_y = 1080 + (540 * mobinValue)
         mob_back_y = 1080 + (540 * mobinValue)
         mob_back2_y = 1080 + (540 * mobinValue)
@@ -80,42 +80,52 @@ function update()
 
     elseif mob_state == 2 then
 
-        mob_out_counter = mob_out_counter + (bpm[0] * deltaTime / 30.0)
-        if mob_out_counter > 1 then
-            mob_state = 0
+        local value = math.min(1, mob_out_counter + (2 * math.abs(state.bpm[0]) * fps.deltaTime / 60.0))
+        if 0 * value == 0 * value then  -- finite
+            mob_out_counter = value
+            if value >= 1 then
+                mob_state = 0
+            end
         end
 
-        mobOutValue = (1 - math.cos(mob_out_counter * math.pi))
+        local mobOutValue = (1 - math.cos(mob_out_counter * math.pi))
         mob_front_y = 1080 + (540 * mobOutValue)
         mob_back_y = 1080 + (540 * mobOutValue)
         mob_back2_y = 1080 + (540 * mobOutValue)
 
     elseif mob_state == 3 then
 
-        mob_counter = mob_counter + (bpm[0] * deltaTime / 60.0)
-        if mob_counter > 1 then
-            mob_counter = 0
+        local value = math.fmod(mob_counter + (math.abs(state.bpm[0]) * fps.deltaTime / 60.0), 1)
+        if 0 * value == 0 * value then  -- finite
+            mob_counter = value
         end
 
 
-        mob_action_counter = mob_action_counter + (bpm[0] * deltaTime / 65.0)
-        if mob_action_counter > 1 then
-            mob_action_counter = 0
+        value = math.fmod(mob_action_counter + (12 / 13.0 * math.abs(state.bpm[0]) * fps.deltaTime / 60.0), 1)
+        if 0 * value == 0 * value then  -- finite
+            mob_action_counter = value
         end
 
 
-        mob_loop_value = (1.0 - math.sin(mob_counter * math.pi))
+        local mob_loop_value = (1.0 - math.sin(mob_counter * math.pi))
         mob_front_y = 1080 + (mob_loop_value * 45)
         mob_back_y = 1080 + (mob_loop_value * 70)
         mob_back2_y = 1080 + (mob_loop_value * 60)
     end
 end
 
-function draw()
+function draw(state)
     if mob_state == 0 then
     else
-        func:DrawGraph(mob_x, mob_back2_y - mob_height, "Mob_Back2.png")
-        func:DrawGraph(mob_x, mob_back_y - mob_height, "Mob_Back.png")
-        func:DrawGraph(mob_x, mob_front_y - mob_height, "Mob_Front.png")
+        tx["Mob_Back2.png"]:Draw(mob_x, mob_back2_y - mob_height)
+        tx["Mob_Back.png"]:Draw(mob_x, mob_back_y - mob_height)
+        tx["Mob_Front.png"]:Draw(mob_x, mob_front_y - mob_height)
     end
+end
+
+function onDestroy()
+    for _, t in pairs(tx) do
+        if t ~= nil then t:Dispose() end
+    end
+    tx = {}
 end
